f1bae8793ddd14a0f2297bd536b4803f313a6b1a
Brigitte Cheetham <Brigitte.Cheetham@mint.co.za>
2017-09-25 16:41:53 +0200

    Add express

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 // This loads the environment variables from the .env file
 require('dotenv-extended').load();
 
-var builder = require('botbuilder');
-var restify = require('restify');
+// var builder = require('botbuilder');
+// var restify = require('restify');
 
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-	console.log("%s listening to %s", server.name, server.url);
-});
+// var server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+// 	console.log("%s listening to %s", server.name, server.url);
+// });
 
-var connector = new builder.ChatConnector({
-	appId: process.env.MICROSOFT_APP_ID,
-	appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+// var connector = new builder.ChatConnector({
+// 	appId: process.env.MICROSOFT_APP_ID,
+// 	appPassword: process.env.MICROSOFT_APP_PASSWORD
+// });
 
-server.post('/api/messages', connector.listen());
+// server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function(session) {
-	session.send("You said: %s", session.message.text);
+// server.post('/', function( req, res) {
+// 	res.render
+// });
+
+// var bot = new builder.UniversalBot(connector, function(session) {
+// 	session.send("You said: %s", session.message.text);
+// });
+
+var express = require('express');
+var app = express();
+var bot = require('./bot');
+
+//bot is really just a rest api so create your route:
+app.post('/api/messages', bot.listen())
+
+var port = process.env.port || process.env.PORT || 3978;
+app.listen(port, function() {
+	console.log('Brigitte says the Web server is listening on port %s', port)
 });

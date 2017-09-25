ec8f84005b1cbd5117f86612014e613a47318672
Brigitte Cheetham <Brigitte.Cheetham@mint.co.za>
2017-09-25 17:26:24 +0200

    Add ejs

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ var bot = require('./bot');
 //bot is really just a rest api so create your route:
 app.post('/api/messages', bot.listen())
 
+app.get('/', function (req, res) {
+	res.render('index.ejs');
+});
+
 var port = process.env.port || process.env.PORT || 3978;
 app.listen(port, function() {
 	console.log('Brigitte says the Web server is listening on port %s', port)

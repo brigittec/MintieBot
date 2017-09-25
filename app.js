1c162f45671f86d40c1489d13c4a1e2e6dd4fe3d
Brigitte Cheetham <Brigitte.Cheetham@mint.co.za>
2017-09-25 08:47:34 +0200

    Add restify

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var app = express();
-var bot = require('./bot');
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
 
-//bot is really just a rest api so create your route:
-app.post('/api/messages/', bot.listen())
+var builder = require('botbuilder');
+var restify = require('restify');
 
-var port = process.env.port || process.env.PORT || 3978;
-app.listen(port, function() {
-	console.log('Brigitte says the Web server is listening on port %s', port)
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+	console.log("%s listening to %s", server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function(session) {
+	session.send("You said: %s", session.message.text);
 });
